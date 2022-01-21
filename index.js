26cbab1a1fb0297c64dd424d79107d059451a988
ertembiyik <ertembiyik@gmail.com>
2022-01-21 17:48:59 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const { readFile } = require("fs").promises
+
+const app = express()
+
+
+app.get("/", async (request, response) => {
+    response.send(await readFile("./home.html", "utf8"))
+})
+
+app.listen(process.env.PORT || 3000, () => console.log("app available on http://localhost:3000"))
